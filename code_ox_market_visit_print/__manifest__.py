ead3e33769937c88cc4340d665ca0bb62f1bb8b3
Shins <shinsmohan276@gmail.com>
2025-02-12 22:11:07 +0530

    [ADDED] custom module for market visit

---
+++ code_ox_market_visit_print/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name':'Market Visit Print Report',
+    'author':'CodeOX',
+    'version':'17.0.1.0',
+    'summary':'',
+    'website':'',
+    'depends':['base','sale_management', 'code_ox_quotation_sales_print'],
+    'data':[
+        'reports/custom_header_footer.xml',
+        'reports/market_visit_template.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
